28a8b781fb414a258bbf52b2bbffcc31cd001f56
Artem <kogay10987@gmail.com>
2024-10-17 18:41:39 +0500

    fix search task

--- src/app/pages/home/filter.worker.ts
+++ src/app/pages/home/filter.worker.ts
@@ -7,15 +7,13 @@ addEventListener('message', (e) => {
   let taskArr = e.data[1];
   const searchKeyword = e.data[2].toLowerCase().trim();
 
-  if (searchKeyword) {
-    taskArr = taskArr.filter(
-      (item: TaskType) =>
-        item.title.toLowerCase().includes(searchKeyword) &&
-        item.status == orderProp
-    );
-  }
+  taskArr = taskArr.filter(
+    (item: TaskType) =>
+      item.title.toLowerCase().includes(searchKeyword) &&
+      item.status == orderProp
+  );
 
-  console.log('Worker',e.data)
+  console.log('Worker', e.data);
 
   postMessage(taskArr);
 });
